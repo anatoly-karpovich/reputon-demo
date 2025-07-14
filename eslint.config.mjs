528582dc422a1acb3fccc7969661fdc74edd7d86
Anatoly Karpovich <anatoly.karpovich90@gmail.com>
2025-07-14 17:17:10 +0200

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["./src/**/*.{js,mjs,cjs,ts}"],
+  },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  {
+    plugins: { prettier: prettierPlugin },
+    rules: {
+      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      "@typescript-eslint/no-unused-expressions": "off",
+      // "max-len": ["off", { code: 120 }],
+      // '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+  {
+    ignores: [
+      "node_modules/**",
+      "dist/**",
+      "playwright-report/**",
+      "husky/**",
+      "test-results/**",
+    ],
+  },
+];
