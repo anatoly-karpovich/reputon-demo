407dd7b8c300ca4ea38828bcc61fb08285cab462
Anatoly Karpovich <anatoly.karpovich90@gmail.com>
2025-07-21 15:33:36 +0200

    fixed removed useless code from globalSetup

--- playwright.config.ts
+++ playwright.config.ts
@@ -57,9 +57,9 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: "on",
     screenshot: "only-on-failure",
-    video: "on-first-retry",
+    video: "retain-on-failure",
   },
 
   /* Configure projects for major browsers */
